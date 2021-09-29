d461ab59d0f58a8bec67624936a656387d3229a9
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2021-09-29 00:24:47 -0600

    animation fixes

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import { Provider } from 'react-redux'
 import Loader from '@/components/Loader'
 
 import store from './store'
-
-const App = lazy(() => import('@/components/App'))
-
+const App = lazy(() =>
+  Promise.all([
+    import('@/components/App'),
+    new Promise((resolve) => setTimeout(resolve, 1000))
+  ]).then(([moduleExports]) => moduleExports)
+)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
