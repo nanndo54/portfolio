39d74a2bb49ccfd610f6e76a5cde383de134578c
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2021-11-04 21:59:52 -0600

    overlaping problem fixed

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,6 +1,3 @@
-import typestyImg1 from '@assets/img/typesty-img1.jpg'
-import typestyImg2 from '@assets/img/typesty-img2.jpg'
-
 import translatorImg1 from '@assets/img/translator-img1.jpg'
 import translatorImg2 from '@assets/img/translator-img2.jpg'
 
@@ -10,13 +7,10 @@ import quizImg2 from '@assets/img/quiz-img2.jpg'
 import jolcImg1 from '@assets/img/jolc-img1.jpg'
 import jolcImg2 from '@assets/img/jolc-img2.jpg'
 
+import typestyImg1 from '@assets/img/typesty-img1.jpg'
+import typestyImg2 from '@assets/img/typesty-img2.jpg'
+
 const projects = [
-  {
-    name: 'Typesty',
-    url: 'https://pabloc54.github.io/typesty/',
-    technologies: ['ReactJS'],
-    images: [typestyImg1, typestyImg2]
-  },
   {
     name: 'Mini Translator',
     url: 'https://pabloc54.github.io/translator/',
@@ -40,6 +34,12 @@ const projects = [
     url: 'https://jolc2.herokuapp.com/',
     technologies: ['ReactJS', 'Redux', 'Python'],
     images: [jolcImg1, jolcImg2]
+  },
+  {
+    name: 'Typesty',
+    url: 'https://pabloc54.github.io/typesty/',
+    technologies: ['ReactJS'],
+    images: [typestyImg1, typestyImg2]
   }
 ]
 
