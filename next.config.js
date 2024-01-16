6e346d32a3bbf40e3265f3f9d354cae8e1ebc650
nanndo54 <nanndo54@outlook.com>
2024-01-16 15:13:28 -0600

    Remove distDir configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: './dist',
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
 
